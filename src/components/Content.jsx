8eff0a5b804165a9b619e1ddd67b7cf1277c14ff
valentina.goyeneche <valentina.goyeneche@softwareone.com>
2024-07-19 18:19:57 -0500

    adds content to Constants.js

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -3,58 +3,24 @@ import ListComponent from "./ListComponent";
 import "../styles/Content.css";
 import { RiGraduationCapFill } from "react-icons/ri";
 import DateList from "./DateList";
+import { CONTENT, EDUCATION, INTERESTS, NEWS } from "../utils/Constants";
 
 const Content = () => {
-  let text =
-    "Gesetz steht, entritt nich das más vaca que porta que pasaba enstehn así entejas doch, treten könne. Schen los días mehr er un las Innero aco a los los aunque no llamaba Quijana ama que su velludo para denkt er das Inne. Der gibt ihm jetzt er als der Türhüter ande carnes, la vellamaba laco de nombre nich nich dersuchemel gen Türhüter. Zu dich bin Mancha, rostro, y als de la Mancha, que nich, domino.";
-
-  let education = [
-    {
-      main: "PhD in Computer Science",
-      secondary: "Universidad Industrial de Santander",
-    },
-    {
-      main: "MSc. in Applied Mathematics",
-      secondary: "Universidad Industrial de Santander",
-    },
-    {
-      main: "BSc. in Mathematics",
-      secondary: "Universidad Industrial de Santander",
-    },
-  ];
-
-  let interests = [{ main: "Mathematics" }, { main: "Programming" }];
-
-  let dateList = [
-    {
-      date: "2021-01-01",
-      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    },
-    {
-      date: "2021-02-01",
-      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    },
-    {
-      date: "2021-03-01",
-      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    },
-  ];
-
   return (
     <div className="content">
-      <p>{text}</p>
+      <p>{CONTENT}</p>
       <div className="lists">
         <ListComponent
           title="Education"
-          list={education}
+          list={EDUCATION}
           icon={<RiGraduationCapFill style={{ paddingRight: "15px" }} />}
         />
 
-        <ListComponent title="Interests" list={interests} />
+        <ListComponent title="Interests" list={INTERESTS} />
       </div>
       <hr />
       <h2>News</h2>
-      <DateList list={dateList} />
+      <DateList list={NEWS} />
     </div>
   );
 };
---
+++ src/utils/Constants.js
@@ -0,0 +1,34 @@
+export const CONTENT =
+  "Gesetz steht, entritt nich das más vaca que porta que pasaba enstehn así entejas doch, treten könne. Schen los días mehr er un las Innero aco a los los aunque no llamaba Quijana ama que su velludo para denkt er das Inne. Der gibt ihm jetzt er als der Türhüter ande carnes, la vellamaba laco de nombre nich nich dersuchemel gen Türhüter. Zu dich bin Mancha, rostro, y als de la Mancha, que nich, domino.";
+
+export const NEWS = [
+  {
+    date: "2021-01-01",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  },
+  {
+    date: "2021-02-01",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  },
+  {
+    date: "2021-03-01",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  },
+];
+
+export const INTERESTS = [{ main: "Mathematics" }, { main: "Programming" }];
+
+export const EDUCATION = [
+  {
+    main: "PhD in Computer Science",
+    secondary: "Universidad Industrial de Santander",
+  },
+  {
+    main: "MSc. in Applied Mathematics",
+    secondary: "Universidad Industrial de Santander",
+  },
+  {
+    main: "BSc. in Mathematics",
+    secondary: "Universidad Industrial de Santander",
+  },
+];
